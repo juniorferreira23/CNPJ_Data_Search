71d2ebc6fe8f5116beb299174c05dcc695e2b227
juniorferreira23 <junior.ferreria@outlook.com>
2023-04-06 16:14:18 -0300

    cli progress

--- teste.js
+++ teste.js
@@ -1,14 +1,14 @@
-function wait(time = 0) {
-    return new Promise((resolve) => {
-      setTimeout(resolve, time);
-    });
-  }
-  
-  // Usando em uma função assíncrona qualquer:
-  async function main() {
-    console.log('Começo.');
-    await wait(5000); // Espera 1,5 segundo (1500ms).
-    console.log('Fim.');
-  }
-  
-  main();
+
+let count = 0
+
+
+
+
+
+// update the current value in your application..
+while(count <= 100){
+  ;
+  count += 10
+}
+
+// stop the progress bar
